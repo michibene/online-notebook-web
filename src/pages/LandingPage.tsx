bdeca600b40eefd5ff6f8598a8c95c3e39b64444
Michal <michal.benoo@gmail.com>
2023-03-03 14:50:42 +0100

    Create basic main page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+    return (
+        <div className="max-w-screen-xl mx-auto min-h-screen">
+            <header>
+                <h1>Your personal ideas notebook</h1>
+            </header>
+        </div>
+    );
+}
