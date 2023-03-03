4128b66b04f480bc0ebef0566f15c02442b6f65e
Michal <michal.benoo@gmail.com>
2023-03-03 14:10:19 +0100

    Add TailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                mainBlack: "#19191b",
+                mainWhite: "#f4f4f5",
+            },
+        },
+    },
+    plugins: [],
+};
