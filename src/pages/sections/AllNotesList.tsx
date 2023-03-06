706106c83cd4a142d109c45e7f326dc26cd99ebf
Michal <michal.benoo@gmail.com>
2023-03-06 10:21:38 +0100

    Update notes cards width

--- src/pages/sections/AllNotesList.tsx
+++ src/pages/sections/AllNotesList.tsx
@@ -7,19 +7,19 @@ type AllNotesListProps = {
 
 export default function AllNotesList({ notesList }: AllNotesListProps) {
     return (
-        <>
+        <div className="flex-grow mb-16">
             {Array.isArray(notesList) && notesList.length ? (
-                <section className="mb-16 grid lg:grid-cols-2 gap-6">
+                <section className="grid lg:grid-cols-2 gap-6">
                     {notesList.map((note: Note) => (
                         <NoteCard key={note.id} title={note.title} body={note.body} dateCreated={note.dateCreated} />
                     ))}
                 </section>
             ) : (
-                <section className="mb-16 space-y-2">
+                <section className="space-y-2">
                     <h2 className="text-customGray font-semibold">No ideas in your notebook</h2>
                     <p>Add a new idea using the form to create a note.</p>
                 </section>
             )}
-        </>
+        </div>
     );
 }
